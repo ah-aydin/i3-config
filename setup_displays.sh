aa6f4f568ef3428dd167b026f18da9936f5f2018
ah-aydin <ahamzaaydin.py@gmail.com>
2024-11-29 18:56:06 +0100

    Update

--- setup_displays.sh
+++ setup_displays.sh
@@ -1,12 +1,12 @@
-if xrandr | grep -q "HDMI-1-0 connected" && xrandr | grep -q "DP-1-1 connected" ; then
+if xrandr | grep -q "HDMI-0 connected" && xrandr | grep -q "DP-1 connected" ; then
   xrandr \
-    --output HDMI-1-0 --mode 1920x1080 --rate 144 --primary \
-    --output eDP-1 --mode 1920x1080 --rate 144 --same-as HDMI-1-0 \
-    --output DP-1-1 --mode 1920x1080 --rate 75 --right-of HDMI-1-0
-elif xrandr | grep -q "HDMI-1-0 connected" ; then
-  xrandr --output HDMI-1-0 --primary --mode 1920x1080 --rate 144 \
-    --output eDP-1 --mode 1920x1080 --rate 144 --same-as HDMI-1-0
+    --output HDMI-0 --mode 2560x1440 --rate 144 --primary \
+    --output DP-1 --mode 1920x1080 --rate 120 --right-of HDMI-0 \
+    --output eDP-1-1 --off
+elif xrandr | grep -q "HDMI-0 connected" ; then
+  xrandr --output HDMI-0 --primary --mode 1920x1080 --rate 144 \
+    --output eDP-1-1 --mode 2560x1440 --rate 144 --same-as HDMI-1-0
 else
-  xrandr --output eDP-1 --auto --output HDMI-1-0 --off
+  xrandr --output eDP-1-1 --auto --output HDMI-1-0 --off
 fi
 
